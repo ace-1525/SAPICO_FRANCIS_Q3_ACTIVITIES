3ff98bff18a2bf2bdba9a30aebae7b9dc951fe05
Francis Ace Sapico <acekimakupadilla28@gmail.com>
2025-12-16 22:15:57 -0800

    Add files via upload

---
+++ FRANCIS_ACE_SAPICO_3/script.js
@@ -0,0 +1,16 @@
+// // Change text content
+document.getElementById("btnText).addEventListener("click", function() {
+    document.getElementById("demo").textContent = "Text change successfully";
+});
+
+//Change text color
+document.getElementById("btnColor").addEventListener("click", function() {
+    myDiv.style.color = "blue";
+
+
+
+    //Show and hide the div
+    document.getElementById("btnHide").addEventListeher("click"< function() {
+        document.getElementById("myDiv").style.display = "none";
+    });
+    
